17c6d72590071e6ddcce39fd035b252edc7efe29
SnowNight2002 <91534622+SnowNight2002@users.noreply.github.com>
2022-07-17 20:09:44 +0800

    Update NYC.java

--- NYC.java
+++ NYC.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+/*
+even numbered streets are east-bound, odd numbered streets are west-bound. 
+Write a Java program to accept a street number from the user and print a message on screen to tell the user the direction. 
+*/
 public class NYC {
    public static void main( String[] args ) {
     Scanner input = new Scanner(System.in);
