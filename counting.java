301ec97a177388e5ae414907d2f75ed35079fc39
SnowNight2002 <91534622+SnowNight2002@users.noreply.github.com>
2022-07-17 20:16:34 +0800

    Update counting.java

--- counting.java
+++ counting.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class m {
+public class counting {
     public static void main( String[] args ) {
         Scanner input = new Scanner(System.in);
        double sum=0;
